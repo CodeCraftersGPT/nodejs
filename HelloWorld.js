2fea0db29ec1be0da491777d9f3a7fa2f99a81a7
Code Crafter <kumar@jatanix.com>
2023-11-05 10:11:24 +0530

    helloWorld

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+// define an employee object with name , age and salary and print the object his name to console
+
+var employee = {
+    name: "John",
+    age: 25,
+    salary: 10000
+};
+
+console.log(employee.name); // John
+console.log(employee.age); // 25
+console.log(employee.salary); // 10000
